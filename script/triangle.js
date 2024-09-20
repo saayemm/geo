2cc960a007ecceb9e68b8e69f312821be304d6df
Saayemm <sayemdcc@gmail.com>
2024-09-20 22:30:04 +0600

    4 work

--- script/triangle.js
+++ script/triangle.js
@@ -14,3 +14,48 @@ function calculateTraingleArea(){
     triAreaspan.innerText = area;
 
 }
+
+function calculateRectangleArea(){
+    const recWideInput = document.getElementById('rec-wide');
+    const recWideText = recWideInput.value;
+    const wide = parseFloat(recWideText);
+
+    const recHeightInput = document.getElementById('rec-height')
+    const recheightText = recHeightInput.value;
+    const height = parseFloat(recheightText);
+
+    const area = wide*height
+
+    const recAreaSpan = document.getElementById('rec-area');
+    recAreaSpan.innerText = area;
+}
+
+function calculatePentaArea(){
+    const pentaInput = document.getElementById('pen-peri');
+    const pentaText = pentaInput.value;
+    const perimeter = parseFloat(pentaText);
+
+    const pentaSideInput = document.getElementById('pen-side');
+    const pentaSideText = pentaSideInput.value;
+    const side = parseFloat(pentaSideText);
+
+    const area = 0.5*perimeter*side;
+
+    const pentaAreaSpan = document.getElementById('pen-area');
+    pentaAreaSpan.innerText = area;
+}
+
+function ellipsAreaCal(){
+    const elipsInput = document.getElementById('elips-peri');
+    const elipsText = elipsInput.value;
+    const a = parseFloat(elipsText);
+
+    const elipsSideInput = document.getElementById('elips-side');
+    const elipsTxt = elipsSideInput.value;
+    const as = parseFloat(elipsTxt);
+    
+    const area = 3.1416*a*as;
+
+    const ellipsAreaSpan = document.getElementById('elips-area');
+    ellipsAreaSpan.innerText = area;
+}
